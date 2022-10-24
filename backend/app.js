5cd4476bfa58dc86b1123470bede8e1cb9f70747
Nikita <script696@yandex.ru>
2022-10-24 08:43:05 +0300

    PR15: fix2 cors 24.10.22

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { errors } = require("celebrate");
-
+const cors = require('./midlewares/cors');
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 
 });
-
+app.use(cors)
 app.use("/", require("./routes/index"));
 
 app.use(errors());

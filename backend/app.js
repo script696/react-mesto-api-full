926a64cc67f9d61354ab9aadc2aa24bcd8cdf3d6
Nikita <script696@yandex.ru>
2022-10-29 11:26:33 +0300

    PR15: add env

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const { errors } = require("celebrate");
 const cors = require('./midlewares/cors');
 const { requestLogger, errorLogger } = require('./midlewares/logger');
+require('dotenv').config()
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.use(bodyParser.json());

da6fe655c1535df0b49d3aa9c5ee0d9432e6378f
Nikita <script696@yandex.ru>
2022-10-29 14:25:25 +0300

    PR15: fix env

--- backend/midlewares/auth.js
+++ backend/midlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const AuthecationError = require("../errors/authecation-error");
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
@@ -12,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, "some-secret-key");
+    payload = jwt.verify(token, NODE_ENV === "production" ? JWT_SECRET : "dev-secret");
   } catch (err) {
     next(new AuthecationError("Необходима авторизация!"));
   }
